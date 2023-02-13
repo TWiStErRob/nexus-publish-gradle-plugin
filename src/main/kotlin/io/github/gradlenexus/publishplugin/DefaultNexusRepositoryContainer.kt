2cdf69b1da17f704e14236f94f4e5cb33a460336
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-13 11:37:53 +0000

    Revert "FAIL: Hackaround by putting the method in the parent extension."
    This reverts commit c9c72e2a17d072b0e198f6964d6c123858f35ae4.

---
+++ src/main/kotlin/io/github/gradlenexus/publishplugin/DefaultNexusRepositoryContainer.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.gradlenexus.publishplugin
+
+import groovy.lang.Closure
+import org.gradle.api.Action
+import org.gradle.api.NamedDomainObjectContainer
+import org.gradle.kotlin.dsl.KotlinClosure1
+import org.gradle.kotlin.dsl.invoke
+import java.net.URI
+import javax.inject.Inject
+
+@Suppress("UnstableApiUsage")
+internal open class DefaultNexusRepositoryContainer @Inject constructor(
+    private val delegate: NamedDomainObjectContainer<NexusRepository>
+) : NexusRepositoryContainer, NamedDomainObjectContainer<NexusRepository> by delegate {
+
+    override fun sonatype(): NexusRepository = sonatype {}
+
+    override fun sonatype(action: Action<in NexusRepository>): NexusRepository = create("sonatype") {
+        nexusUrl.set(URI.create("https://oss.sonatype.org/service/local/"))
+        snapshotRepositoryUrl.set(URI.create("https://oss.sonatype.org/content/repositories/snapshots/"))
+        action.execute(this)
+    }
+
+    override fun configure(configureClosure: Closure<*>): NamedDomainObjectContainer<NexusRepository> =
+        delegate.configure(KotlinClosure1<NexusRepositoryContainer, Any>({ configureClosure(this) }, this, this))
+}
